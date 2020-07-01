2169198912c9222311a8ac69bb81dab47606cd22
Renan Greca <renangreca@gmail.com>
2020-07-01 12:41:44 +0200

    Improvements to clear button and code organization in GroceryList

--- GroceryCalculator/Views/GroceryList.swift
+++ GroceryCalculator/Views/GroceryList.swift
@@ -11,14 +11,15 @@ import WatchConnectivity
 
 struct GroceryList: View {
     
-//    @State var addItemPopUpVisible = false
-//    @State var buyItemPopUpVisible = false
-//    @State var selectedGrocery: GroceryItem?
+    //    @State var addItemPopUpVisible = false
+    //    @State var buyItemPopUpVisible = false
+    //    @State var selectedGrocery: GroceryItem?
     
     @ObservedObject var keyboard = KeyboardResponder()
-//    @EnvironmentObject var groceryItems: GroceryItems
-//    @State private var editMode = EditMode.inactive
-        
+    @State var showingAlert = false
+    //    @EnvironmentObject var groceryItems: GroceryItems
+    //    @State private var editMode = EditMode.inactive
+    
     @Environment(\.managedObjectContext) var context
     @FetchRequest(entity: GroceryItemMO.entity(),
                   sortDescriptors: [NSSortDescriptor(key: "position", ascending: true)],
@@ -29,66 +30,36 @@ struct GroceryList: View {
     var body: some View {
         let total = fetchedGroceries.reduce(0) { $0 + $1.price }
         let totalPrice = currencyFormatter.string(for: total) ?? "0"
-
+        
         return ZStack {
             // NavigationView including the list of groceries
             NavigationView {
                 VStack {
-                    List {
-                        ForEach(fetchedGroceries, id: \.self, content: { groceryMO in
-                            ListRow(groceryItem: groceryMO)
-                        })
-                        .onDelete() { indexSet in
-                            let grocery = self.fetchedGroceries[indexSet.first!]
-                            self.context.delete(grocery)
-                            try? self.context.save()
-                        }
-                        .onMove() { source, destination in
-                            var list = self.fetchedGroceries.compactMap() { $0 }
-                            
-                            list.move(fromOffsets: source, toOffset: destination)
-
-                            for i in 0..<list.count {
-                                list[i].updatePosition(index: i)
-                            }
-                            
-                            try? self.context.save()
-                        }
-
-                        // Blank row for adding new grocery
-                        NewGroceryRow(position: self.fetchedGroceries.count)
-                    }
+                    GroceryList()
                     // Row showing total value of purchase
                     if (keyboard.currentHeight == 0) {
                         TotalRow(totalPrice: totalPrice)
                     }
                 }
-//                .environment(\.editMode, $editMode)
-                .navigationBarTitle(Text("Grocery List"))
-                .navigationBarItems(leading: Button(action: {
-                    for item in self.fetchedGroceries {
-                        self.context.delete(item)
-                    }
-                    try? self.context.save()
-//                    self.groceryItems.clear()
-                }, label: {
-                    Text("Clear")
-                }), trailing: EditButton())
-                .padding(.bottom, keyboard.currentHeight-35)
-//                .edgesIgnoringSafeArea(.bottom)
-                .animation(.easeInOut(duration: 0.16))
+                    //                .environment(\.editMode, $editMode)
+                    .navigationBarTitle(Text("Grocery List"))
+                    .navigationBarItems(leading: ClearButton(),
+                                        trailing: EditButton())
+                    .padding(.bottom, keyboard.currentHeight-35)
+                    //                .edgesIgnoringSafeArea(.bottom)
+                    .animation(.easeInOut(duration: 0.16))
             }
             .navigationViewStyle(StackNavigationViewStyle())
         }
     }
     
     private func onDelete(offsets: IndexSet) {
-//        groceryItems.remove(at: offsets)
+        //        groceryItems.remove(at: offsets)
         
     }
-
+    
     private func onMove(source: IndexSet, destination: Int) {
-//        groceryItems.move(from: source, to: destination)
+        //        groceryItems.move(from: source, to: destination)
     }
     
     private func leadingPadding() -> CGFloat {
@@ -98,21 +69,66 @@ struct GroceryList: View {
         return 0
     }
     
-//    func showBuyItemPopUp(groceryItem: GroceryItem) {
-//        self.selectedGrocery = groceryItem
-//    }
+    
+    func ClearButton() -> some View {
+        Button(action: {
+            self.showingAlert.toggle()
+        }, label: {
+            Image(systemName: "trash")
+                .foregroundColor(.red)
+        }).alert(isPresented: $showingAlert) {
+            Alert(title: Text("Warning"),
+                  message: Text("Are you sure you want to clear yout list?"),
+                  primaryButton: .cancel(Text("Cancel")),
+                  secondaryButton: .destructive(Text("Delete")) {
+                    for item in self.fetchedGroceries {
+                        self.context.delete(item)
+                    }
+                    try? self.context.save()
+                    self.showingAlert = false
+                })
+        }
+    }
+    
+    func GroceryList() -> some View {
+        List {
+            ForEach(fetchedGroceries, id: \.self, content: { groceryMO in
+                ListRow(groceryItem: groceryMO)
+            })
+            .onDelete() { indexSet in
+                    let grocery = self.fetchedGroceries[indexSet.first!]
+                    self.context.delete(grocery)
+                    try? self.context.save()
+            }
+            .onMove() { source, destination in
+                var list = self.fetchedGroceries.compactMap() { $0 }
+                
+                list.move(fromOffsets: source, toOffset: destination)
+                
+                for i in 0..<list.count {
+                    list[i].updatePosition(index: i)
+                }
+                
+                try? self.context.save()
+            }
+            
+            // Blank row for adding new grocery
+            NewGroceryRow(position: self.fetchedGroceries.count)
+        }
+    }
+
 }
 
 struct GroceryList_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-        GroceryList()//.environmentObject(GroceryItems())
-            .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro"))
-            .previewDisplayName("iPhone 11 Pro")
-        
-        GroceryList()//.environmentObject(GroceryItems())
-            .previewDevice(PreviewDevice(rawValue: "iPad Pro (11-inch) (2nd generation)"))
-            .previewDisplayName("iPad Pro (11-inch)")
+            GroceryList()//.environmentObject(GroceryItems())
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro"))
+                .previewDisplayName("iPhone 11 Pro")
+            
+            GroceryList()//.environmentObject(GroceryItems())
+                .previewDevice(PreviewDevice(rawValue: "iPad Pro (11-inch) (2nd generation)"))
+                .previewDisplayName("iPad Pro (11-inch)")
         }
     }
 }
@@ -120,23 +136,23 @@ struct GroceryList_Previews: PreviewProvider {
 final class KeyboardResponder: ObservableObject {
     private var notificationCenter: NotificationCenter
     @Published private(set) var currentHeight: CGFloat = 0
-
+    
     init(center: NotificationCenter = .default) {
         notificationCenter = center
         notificationCenter.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
+    
     deinit {
         notificationCenter.removeObserver(self)
     }
-
+    
     @objc func keyBoardWillShow(notification: Notification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             currentHeight = keyboardSize.height// - 110
         }
     }
-
+    
     @objc func keyBoardWillHide(notification: Notification) {
         currentHeight = 0
     }
